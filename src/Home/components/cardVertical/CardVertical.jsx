f45a0f018cd9a46de10f32d1753c9e5f920afe11
Catherine -marin <katerin243@hotmail.com>
2023-06-18 22:20:20 +0200

    cambios en  card

--- src/Home/components/cardVertical/CardVertical.jsx
+++ src/Home/components/cardVertical/CardVertical.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import "../../styles/CardVertical.css"
+import CheckIcon from '@mui/icons-material/Check';
 
+export const CardVertical = ({ imageUrl, title, descriptionItems }) => {
+    return (
+        <div className="card-wrapper">
+            <div className="div-image">
+                <img className="img-thumbnail" src={imageUrl} alt={title} />
+            </div>
+            <div className="card">
+                <div className="card-body content-Card">
+                    <h3 className="card-title">{title}</h3>
+                    <ul className="description-list">
+                        {descriptionItems.map((item, index) => (
+                            <li key={index}>
+                                <CheckIcon style={{ color: 'white', marginRight: '10px', fontSize: 20 }}/>
+                                {item}
+                                <br />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
 
  
-export const CardVertical = ({ imageUrl, title, descriptionItems }) => {
+/* export const CardVertical = ({ imageUrl, title, descriptionItems }) => {
     return (
         
         <div className="card-wrapper">
@@ -27,7 +51,7 @@ export const CardVertical = ({ imageUrl, title, descriptionItems }) => {
             </div>
         </div>
     );
-};
+}; */
 
 
 

4ff4ccc62ac98c022fbe8754f852e1fe8bb2f14e
FranB201 <fran.bolufer@hotmail.com>
2023-06-29 19:47:05 +0200

    cambios

--- src/Home/components/index.js
+++ src/Home/components/index.js
@@ -5,3 +5,5 @@ export * from './cardReviews/CardReview';
 export * from './cardVertical/CardVerticalFact';
 export * from './sideBar/SideBar';
 export * from './carousel/Carousel';
+export * from './comingSoon/ComingSoonVideos';
+export * from './headerImage/Headerimage';

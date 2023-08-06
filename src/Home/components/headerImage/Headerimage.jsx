25db9ab847821cc67456c587aa4a9eb3684c8400
Catherine -marin <katerin243@hotmail.com>
2023-08-06 17:36:25 +0200

    cambios en el header principal

--- src/Home/components/headerImage/Headerimage.jsx
+++ src/Home/components/headerImage/Headerimage.jsx
@@ -13,9 +13,10 @@ import OLGAFIN from '../../../../src/assets/HomeImage/homeimg5.jpg';
 import STUDIO2 from '../../../../src/assets/studioImages/studio2.jpg';
 export const HeaderImage = ({ title, motivationText, presentation1, presentation2, buttonText }) => {
     return (
-       
+<div className="carousel-container" style={{ position: 'relative' }}>     
 <div  className="carousel slide position-relative" data-bs-ride="carousel">
     <div className="carousel-inner">
+    {title}
     
         <div className="carousel-item active" data-bs-interval="5000">
             <img src={AITORFIN} className='w-100 carousel-image'/>
@@ -34,7 +35,14 @@ export const HeaderImage = ({ title, motivationText, presentation1, presentation
         </div>
     </div>
 </div>
-
+    <div className="carousel-description" >
+        <h3 className="motivation-text title-img-text-header">{title}</h3>
+        <p className="motivation-text sentence">{motivationText}</p>
+        <p className="motivation-text footer-img-text-header">{presentation1}</p>
+        
+        <ModalForm></ModalForm>
+    </div>
+</div>
 
     );
 };

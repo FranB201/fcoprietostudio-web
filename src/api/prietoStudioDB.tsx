221d44e0fd8c22058349eb07cbbc209788d66203
Catherine -marin <katerin243@hotmail.com>
2023-09-05 18:32:09 +0200

    endpoint api

--- src/api/prietoStudioDB.tsx
+++ src/api/prietoStudioDB.tsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 const prietoStudioDB = axios.create({
-
-    baseURL:'http://localhost:8080/api'
-
-
+    baseURL:'http://93.93.117.148:8080/api'
 
 });
 
 
+    /* baseURL:'http://localhost:8080/api' */
 export default prietoStudioDB;

69d2fd7dbcb1c2ddbb9351051a44c93fea5e563d
Catherine -marin <katerin243@hotmail.com>
2023-09-21 20:56:01 +0200

    cambio de componente de carrousel

--- src/Home/components/headerImage/Headerimage.jsx
+++ src/Home/components/headerImage/Headerimage.jsx
@@ -1,59 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+
 import "../../styles/CardVertical.css";
 import "../../styles/HeaderImage.css";
 import { ModalForm } from '../ModalForm/ModalForm';
 
 import homeImg from '../../../../src/assets/HomeImage/homeImg5.jpg';
 import STUDIO2 from '../../../../src/assets/HomeImage/entradaCentro.jpg';
-import entradaImg from '../../../../src/assets/HomeImage/entradaCentro.jpg';
+import entradaImg from '../../../../src/assets/studioImages/studio2.jpg';
 
 export const HeaderImage = ({ title, motivationText, presentation1, presentation2, buttonText }) => {
-
-
+    const images = [STUDIO2, homeImg,entradaImg];
+    const [currentIndex, setCurrentIndex] = useState(0);
     
     useEffect(() => {
-        const carouselElement = document.querySelector('.carousel');
-        if (carouselElement) {
-            new bootstrap.Carousel(carouselElement, {
-                interval: 3000
-            });
-        }
-    }, []);
-    
-    
+        console.log('images.length:', images.length); // Verifica la longitud de tu array de imágenes
+        const intervalId = setInterval(() => {
+            console.log('Changing Image'); // Este se imprimirá cada vez que se cambie la imagen
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 3000);
+        return () => clearInterval(intervalId);
+    }, [images.length]);
+
+    console.log('Rendering with currentIndex:', currentIndex); // Este se imprimirá cada vez que se renderice el componente
 
     return (
-<div className="carousel-container" style={{ position: 'relative' }}>
-    <div className="carousel slide position-relative">
-        <div className="carousel-inner">
-            {title}
-
-            <div className="carousel-item active" data-bs-interval="3000">
-                <img src={homeImg}  className='w-100 carousel-image' alt="Home" />
+        <div className="carousel-container" style={{ position: 'relative' }}>
+            <img src={images[currentIndex]} className='w-100 carousel-image' alt="Home" />
+            <div className="carousel-description">
+                <h3 className="motivation-text title-img-text-header">{title}</h3>
+                <p className="motivation-text sentence">{motivationText}</p>
+                <p className="motivation-text footer-img-text-header">{presentation1}</p>
             </div>
-
-            <div className="carousel-item" data-bs-interval="3000">
-                <img src={entradaImg}  className='w-100 carousel-image' alt="Home" />
-            </div>
-
         </div>
-
-        {/* Agregar las flechas de navegación aquí */}
-
-        <button className="carousel-control-next" type="button" data-bs-target=".carousel" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-        </button>
-
-    </div>
-    <div className="carousel-description">
-        <h3 className="motivation-text title-img-text-header">{title}</h3>
-        <p className="motivation-text sentence">{motivationText}</p>
-        <p className="motivation-text footer-img-text-header">{presentation1}</p>
-
-        <ModalForm></ModalForm>
-    </div>
-</div>
-
     );
 };

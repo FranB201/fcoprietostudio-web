f734f5b7fc7e232c9ac70dbb7162196c0893000f
FranB201 <fran.bolufer@hotmail.com>
2023-11-05 18:34:07 +0100

    cambios

--- src/Home/pages/InfoContactPage.jsx
+++ src/Home/pages/InfoContactPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HomeLayout } from '../layout/HomeLayout'
-import entradaCentro from '../../../src/assets/HomeImage/entradaCentro.jpg'
+import entradaCentro from '../../../src/assets/HomeImage/entradaCentro2.png'
 import "../styles/InfoContactPageStyle.css"
 export const InfoContactPage = () => {
   return (
@@ -11,23 +11,31 @@ export const InfoContactPage = () => {
           <div className="row">
 
             <div className=" p-5">
-              <h2>Hola</h2>
+              <h2>Tu Salud y Condición Física es Nuestra Prioridad</h2>
               <div className="centered-content">
 
-            <h4>Tu Salud y Condición Física es Nuestra Prioridad</h4>
-              <p>
-                  En nuestro centro, no somos simplemente un gimnasio. Somos un equipo integrado por Entrenadores, Nutricionistas, Quiromasajistas y Fisioterapeutas, comprometidos en ofrecer un entrenamiento personalizado que atiende tus necesidades individuales.
-              </p>
-              <p>
-                  Nuestro enfoque no se basa en las tendencias pasajeras. En lugar de eso, nos tomamos el tiempo para escuchar y evaluar a cada cliente, garantizando así un plan adaptado a tus objetivos, todo ello en un entorno profesional y responsable.
-              </p>
-              <p>
-                  Este compromiso se ve respaldado por más de 30 años de experiencia en el entrenamiento personal bajo la dirección de Francisco Prieto y nuestra constante búsqueda de formación y mejoramiento.
-              </p>
-          </div>
-      </div>
+{/*                 <h4>Tu Salud y Condición Física es Nuestra Prioridad</h4>
+ */}                <p>
+                  En <strong>FranciscoPrieto Studio</strong>, nuestra pasión es la actividad física y el deporte, y eso se refleja en nuestro compromiso contigo. No somos simplemente un gimnasio, somos un <strong>Centro Integral de Entrenamiento Personal</strong> donde la experiencia y la personalización son la esencia de nuestro servicio.
+                </p>
+                <p>
+                  Dirigido por <strong>Francisco Prieto</strong>, un profesional con formación en fisicoculturismo y musculación desde 1987 y entrenamiento personal desde 1991, nuestro centro cuenta con más de 30 años de experiencia. Francisco no solo ha ayudado a clientes a alcanzar sus metas, sino que también ha competido al más alto nivel, organizado eventos significativos y formado a cientos de instructores.
+                </p>
+                <p>
+                  Somos un equipo integrado por <strong>Entrenadores, Nutricionistas y Fisioterapeutas</strong>, todos trabajando unidos para ofrecerte un servicio personalizado y completo. Nos tomamos el tiempo para escuchar y evaluar tu situación individual y diseñamos un entrenamiento a medida de la forma más profesional y responsable posible.
+                </p>
+                <p>
+                  Nos enorgullece nuestro enfoque único, lejos de modas y tendencias pasajeras, orientado en resultados a largo plazo. La pasión de Francisco por el aprendizaje y la mejora continua se traduce en una oferta educativa de calidad para nuestros clientes y su constante crecimiento personal y físico.
+                </p>
+                <p>
+                  Con <strong>FranciscoPrieto Studio</strong>, te acompañamos en cada paso de tu camino hacia el éxito, asegurándonos de que cada aspecto de tu salud y condición física sea atendido con la máxima calidad y atención personal.
+                </p>
 
-      {/*       <div className="icons-line-nutrition">
+              </div>
+
+            </div>
+
+            {/*       <div className="icons-line-nutrition">
           <span className="material-symbols-outlined" style={{ fontSize: 60, color: 'white' }}>call</span>
           <span style={{ color: 'white', fontSize: '24px', marginLeft: '10px' }}>+34 693 02 32 21</span> 
           <span className="material-symbols-outlined" style={{ fontSize: 60, color: 'white', marginLeft: '30px' }}>email</span>
@@ -36,38 +44,28 @@ export const InfoContactPage = () => {
 
 
           </div>
-          <div className='row'>
-
-
-          <div className="background-sections-nutrition-borderless" style={{
+          <div class='row'>
+            <div class="background-sections-nutrition-borderless" style={{
               backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.9)), url(${entradaCentro})`,
-              backgroundSize: 'cover',  // Cambiado a 'cover' para que la imagen cubra el espacio disponible sin distorsionar
+              backgroundSize: 'cover',  
               backgroundRepeat: 'no-repeat',
               backgroundPosition: 'center'
             }}>
-
-              <div className="container d-flex justify-content-center mt-5">
-                  <div className="list-group mx-auto custom-width mt-5">
-                      <a href="#" className="list-group-item list-group-item-action active">Horario del centro</a>
-                      <a href="#" className="list-group-item list-group-item-action">Lunes: 06:00am - 13:00pm | 17:00pm - 21:00pm</a>
-                      <a href="#" className="list-group-item list-group-item-action">Martes: 06:00am - 13:00pm | 17:00pm - 21:00pm</a>
-                      <a href="#" className="list-group-item list-group-item-action">Miércoles: 06:00am - 13:00pm | 17:00pm - 21:00pm</a>
-                      <a href="#" className="list-group-item list-group-item-action">Jueves: 06:00am - 13:00pm | 17:00pm - 21:00pm</a>
-                      <a href="#" className="list-group-item list-group-item-action">Viernes: 06:00am - 13:00pm | 17:00pm - 20:00pm</a>
-                      <a href="#" className="list-group-item list-group-item-action">Sábado: Cerrado</a>
-                      <a href="#" className="list-group-item list-group-item-action">Domingo: Cerrado</a>
-                  </div>
+              <div class="container d-flex justify-content-center mt-5">
+                <div class="list-group mx-auto custom-width mt-5">
+                  <div class="list-group-item-header">Horario del centro</div>
+                  <div class="list-group-item">Lunes: 06:00am - 13:00pm | 17:00pm - 21:00pm</div>
+                  <div class="list-group-item">Martes: 06:00am - 13:00pm | 17:00pm - 21:00pm</div>
+                  <div class="list-group-item">Miércoles: 06:00am - 13:00pm | 17:00pm - 21:00pm</div>
+                  <div class="list-group-item">Jueves: 06:00am - 13:00pm | 17:00pm - 21:00pm</div>
+                  <div class="list-group-item">Viernes: 06:00am - 13:00pm | 17:00pm - 20:00pm</div>
+                  <div class="list-group-item disabled">Sábado: Cerrado</div>
+                  <div class="list-group-item disabled">Domingo: Cerrado</div>
+                </div>
               </div>
-
-
             </div>
-
-
           </div>
-
-
         </div>
-
       </div>
 
     </HomeLayout>
--- src/Home/pages/StudiosTrainings.jsx
+++ src/Home/pages/StudiosTrainings.jsx
@@ -5,7 +5,7 @@ import grupxpressCard from "../../../src/assets/cardImages/grupoXpresSala.jpg";
 import personaltrainingCard from "../../../src/assets/cardImages/entrenamientoP2.jpg";
 import studio1 from "../../../src/assets/backgrounds/studio1.jpg";
 import appStudio from "../../../src/assets/nutritionImages/appStudio.png";
-import sixtyDays from "../../../src/assets/cardImages/PASTILLARETOsinfondo.png";
+import sixtyDays from "../../../src/assets/cardImages/PastillaRetoCard.png";
 import "../../../src/Home/styles/StudiosTrainingStyle.css";
 import { FormOnly } from "../components/ModalForm/FormOnly";
 
@@ -62,12 +62,12 @@ export const StudiosTrainings = () => {
             <div id="personaltraining" className="col-lg-3 col-md-6 col-sm-12">
               <CardVertical
                 imageUrl={sixtyDays}
-                title="Entrenamiento Personal"
+                title="Reto 60 días"
                 descriptionItems={[
-                  "Entrenamiento personalizado 100%",
-                  "Resultado garantizado",
-                  "Revisiones constantes",
-                  "Plan nutricional incluido"
+                  "Seguimiento nutricional",
+                  "Mediciones semanales",
+                  "Soporte y motivación",
+                  "Entrenamientos personalizados"
                 ]}
               />
             </div>
